c899154c999d356a45e98d7a6f5f8c50848f7a3f
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-28 19:58:49 +0530

    added image animation

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -1,27 +1,77 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+
 import React, { useRef } from "react";
 
 const Agency = () => {
   gsap.registerPlugin(ScrollTrigger);
-  const imageDivRef = useRef(null);
 
-  useGSAP(() => {
-    // gsap.to(imageDivRef.current, {
-    //   scrollTrigger: {
-    //     trigger: imageDivRef,
-    //     markers: true,
-    //   },
-    // });
+  const imageDivRef = useRef(null);
+  const imageRef = useRef(null);
+
+  // useGSAP(() => {
+  //   gsap.to(imageDivRef.current,{
+  //     scrollTrigger:{
+  //       trigger: imageDivRef,
+  //       markers: true,
+  //       start: "top 36%",
+  //       end: "top -100%",
+  //       pin: true,
+  //       scrub: true,
+  //     },
+  //   });
+  // });
+
+  const imgArray = [
+    "public/img/img1.jpeg",
+    "public/img/img2.jpeg",
+    "public/img/img3.jpeg",
+    "public/img/img4.jpeg",
+    "public/img/img5.jpeg",
+    "public/img/img6.jpeg",
+    "public/img/img7.jpeg",
+    "public/img/img8.jpeg",
+    "public/img/img9.jpeg",
+    "public/img/img10.jpeg",
+    "public/img/img11.jpeg",
+    "public/img/img12.jpeg",
+    "public/img/img13.jpeg",
+  ];
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        // markers: true,
+        start: "top 21.9%",
+        end: "top -100%",
+        scrub: true,
+        pin: true,
+        onUpdate: (e) => {
+          let imgIndex;
+          if (e.progress < 1) {
+            imgIndex = Math.round(e.progress * imgArray.length);
+          } else {
+            imgIndex = imgArray.length - 1;
+          }
+
+          imageRef.current.src = imgArray[imgIndex];
+          // console.log(imageRef.current.src);
+        },
+      },
+    });
   });
 
   return (
     <div>
-      <div className="section1">
-        <div className="img h-80 -z-10 top-40 left-112  absolute w-60">
+      <div className="section1  ">
+        <div
+          ref={imageDivRef}
+          className="img h-80 -z-10 top-40 left-112  absolute w-60"
+        >
           <img
-            ref={imageDivRef}
+            ref={imageRef}
             className="h-full w-full rounded-4xl  object-cover "
             src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
             alt="personImg"
