9492584936a1b8d52db731e6e772dca2fa2f76d7
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-28 14:34:39 +0530

    completed home page footer  work

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='flex text-[6.5vw] items-end justify-center gap-8 text-white  font-[font2]' >
-      
+      <Link className=' border-5 leading-[6vw] pt-2 px-8 border-white rounded-full  uppercase  ' to="" >Work</Link>
+      <Link className=' border-5 leading-[6vw] pt-2 px-8 border-white rounded-full  uppercase  ' to="" >Agency</Link>
     </div>
   )
 }
--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -3,6 +3,7 @@ import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
+    <>
     <div className="pt-5 text-center font-[font1] text-[10vw] text-white">
       <div className="  leading-[8vw]  flex items-start  justify-center  uppercase  font-bold ">
         The spark for
@@ -17,7 +18,18 @@ const HomeHeroText = () => {
       <div className="  leading-[8vw] flex items-start  justify-center   uppercase  font-bold  ">
         creative
       </div>
+      
     </div>
+    <div className="flex w-screen items-center justify-end">
+        <p className="text-end" >
+          K72 is an agency that builds brands from every angle. Today, tomorrow
+          and years from now. We think the best sparks fly when comfort zones
+          get left behind and friction infuses our strategies, brands and
+          communications with real feeling. We’re transparent, honest and say
+          what we mean, and when we believe in something, we’re all in.
+        </p>
+      </div>
+    </>
   );
 };
 
