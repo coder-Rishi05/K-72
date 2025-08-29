9d9741439f768e257cdf7ada5b627b34b314476a
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-29 10:43:03 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Agency from "./pages/Agency";
+import Navbar from "./Navigation/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
