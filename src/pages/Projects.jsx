be110ff8a8a46e4360d23ba02f2d69c859732d13
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-31 16:40:24 +0530

    started project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <div className="">
+        <h2 className='font-[font1] uppercase text-[15vw] font-bold ' >Projets</h2>
+      </div>
+    </div>
   )
 }
 
