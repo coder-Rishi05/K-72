70b9ffac78a3799d9a89cf3b1bc5ec3f87ce77a3
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-01 21:55:20 +0530

    added all the images to the pojets

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
   return (
     <div>
       <div className="w-full  h-[700px] mb-4 flex gap-4 ">
         <div className="w-1/2  group  transition-all duration-200 cursor-pointer hover:rounded-[50px] relative overflow-hidden h-full ">
           <img
             className="w-full h-full  object-cover object-center "
-            src="public/projets_img/img1.jpg"
+            src={props.img.imgSrc}
             alt=""
           />
           <div className=" opacity-0 group-hover:opacity-100 transition-all duration-200 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10">
             <h2 className=" border-4 border-white rounded-full  uppercase font-[font1] font-bold text-white leading-[3vw]    text-center  pt-4 px-8  text-6xl ">
-              Voir le projet
+              {props.img.tag}
             </h2>
           </div>
         </div>
         <div className="w-1/2  group  transition-all duration-200 cursor-pointer hover:rounded-[50px] relative overflow-hidden h-full ">
           <img
             className="w-full h-full  object-cover object-center "
-            src="public/projets_img/img1.jpg"
+            src={props.img.imgSrc}
             alt=""
           />
           <div className=" opacity-0 group-hover:opacity-100 transition-all duration-200 absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10">
             <h2 className=" border-4 border-white rounded-full  uppercase font-[font1] font-bold text-white leading-[3vw]    text-center  pt-4 px-8  text-6xl ">
-              Voir le projet
+              {props.img.tag}
             </h2>
           </div>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,18 +1,84 @@
 import React from "react";
+import img1 from "/img1.jpg";
+import img2 from "/img2.jpg";
+import img3 from "/img3.jpg";
+import img4 from "/img4.jpg";
+import img5 from "/img5.jpg";
+import img6 from "/img6.jpg";
+import img7 from "/img7.jpg";
+import img8 from "/img8.jpg";
+import img9 from "/img9.jpg";
+import img10 from "/img10.jpg";
+import img11 from "/img11.pg";
+import img12 from "/img12.jpg";
+import ProjectCard from "../components/Projects/ProjectCard";
 
 const Projects = () => {
+  const ImgArray = [
+    {
+      imgSrc: img1,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img2,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img3,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img4,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img5,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img6,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img7,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img8,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img9,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img10,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img11,
+      tag: "Voir le projet",
+    },
+    {
+      imgSrc: img12,
+      tag: "Voir le projet",
+    },
+  ];
+
   return (
     <div className="p-4">
       <div className=" pt-[45vh]">
         <h2 className="font-[font2] relative uppercase text-[10.5vw] font-bold ">
-          Projets{" "}
+          Projets
           <sup className="text-4xl absolute top-14 font-bold font-[font2] ">
             16
           </sup>
         </h2>
       </div>
       <div className=" -mt-10">
-        
+        {ImgArray.map((item, index) => (
+          <ProjectCard img={item} key={index} />
+        ))}
       </div>
     </div>
   );
