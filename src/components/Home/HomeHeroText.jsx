38849b7bf119e695189e7021a42075b815302c92
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-02 15:40:32 +0530

    fixed homepage responsiveness

--- src/components/Home/HomeHeroText.jsx
+++ src/components/Home/HomeHeroText.jsx
@@ -4,29 +4,29 @@ import Video from "./Video";
 const HomeHeroText = () => {
   return (
     <>
-    <div className="pt-5 text-center font-[font1] text-[10vw] text-white">
-      <div className="  leading-[8vw]  flex items-start  justify-center  uppercase  font-bold ">
-        The spark for
-      </div>
-      <div className="  flex items-start  justify-center leading-[8vw]  uppercase  font-bold ">
-        all
-        <div className=" h-[7vw] w-[13vw] rounded-full   overflow-hidden ">
-          <Video />
+      <div className="pt-5 text-center font-[font1] text-[12vw] mt-72 lg:mt0 lg:text-[10vw] text-white">
+        <div className="  lg:leading-[8vw] leading-[10vw]  flex items-start  justify-center  uppercase  font-bold ">
+          The spark for
         </div>
-        things
+        <div className="  flex items-start  justify-center lg:leading-[8vw] leading-[10vw]  uppercase  font-bold ">
+          all
+          <div className=" lg:h-[7vw] h-[9vw] lg:w-[13vw] w-[15vw] rounded-full   overflow-hidden ">
+            <Video />
+          </div>
+          things
+        </div>
+        <div className="  lg:leading-[8vw] leading-[10vw] flex items-start  justify-center   uppercase  font-bold  ">
+          creative
+        </div>
       </div>
-      <div className="  leading-[8vw] flex items-start  justify-center   uppercase  font-bold  ">
-        creative
-      </div>
-      
-    </div>
-    <div className="flex w-screen items-center justify-end">
-        <p className="  text-white font-bold text-sm w-[20vw]" >
-          <span className="ml-10" >K72 is an agency that builds brands</span> from every angle. Today, tomorrow
-          and years from now. We think the best sparks fly when comfort zones
-          get left behind and friction infuses our strategies, brands and
-          communications with real feeling. We’re transparent, honest and say
-          what we mean, and when we believe in something, we’re all in.
+      <div className="flex w-screen items-end justify-end">
+        <p className="text-white font-bold text-xs lg:text-sm w-[75vw] lg:w-[20vw]">
+          <span className="ml-10">K72 is an agency that builds brands</span>{" "}
+          from every angle. Today, tomorrow and years from now. We think the
+          best sparks fly when comfort zones get left behind and friction
+          infuses our strategies, brands and communications with real feeling.
+          We’re transparent, honest and say what we mean, and when we believe in
+          something, we’re all in.
         </p>
       </div>
     </>
